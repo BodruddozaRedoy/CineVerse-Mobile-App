6df7e2df5c4a794f94ff15c9a805fb9022a3f169
bodruddozaredoy <bodruddozaredoy@gmail.com>
2025-08-20 06:30:28 +0600

    d

---
+++ services/api.ts
@@ -0,0 +1,22 @@
+export const  TMDB_CONFIG = {
+    BASE_URL: "https://api.themoviedb.org/3",
+    API_KEY: process.env.EXPO_PUBLIC_API_KEY,
+    headers: {
+        accept: "application/json",
+        Authorization: ""
+    }
+}
+
+const url = '/movie/movie_id?language=en-US';
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MDg2NzdmM2RjZjlmYzQ4OWVjOTE5Yzc3ZGU3OWVmZiIsIm5iZiI6MTc1NTY0OTAzMS42MjcsInN1YiI6IjY4YTUxNDA3M2EwMWMxYzNlNmQ1NDljMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5hyWXoXZmeh0Zgh9z2zfckICA-9YeyKX_DCOCxvJ470'
+  }
+};
+
+fetch(url, options)
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => console.error(err));
