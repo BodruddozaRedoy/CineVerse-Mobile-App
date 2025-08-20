989a85c1cab99e63adc002d6ba7fceddface0995
bodruddozaredoy <bodruddozaredoy@gmail.com>
2025-08-21 05:47:08 +0600

    d

--- services/api.ts
+++ services/api.ts
@@ -1,22 +1,26 @@
-export const  TMDB_CONFIG = {
-    BASE_URL: "https://api.themoviedb.org/3",
-    API_KEY: process.env.EXPO_PUBLIC_API_KEY,
-    headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
-    }
-}
-
-const url = '/movie/movie_id?language=en-US';
-const options = {
-  method: 'GET',
+export const TMDB_CONFIG = {
+  BASE_URL: "https://api.themoviedb.org/3",
+  API_KEY: process.env.EXPO_PUBLIC_API_KEY,
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MDg2NzdmM2RjZjlmYzQ4OWVjOTE5Yzc3ZGU3OWVmZiIsIm5iZiI6MTc1NTY0OTAzMS42MjcsInN1YiI6IjY4YTUxNDA3M2EwMWMxYzNlNmQ1NDljMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5hyWXoXZmeh0Zgh9z2zfckICA-9YeyKX_DCOCxvJ470'
-  }
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+  },
 };
 
-fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .catch(err => console.error(err));
+export const fetchMovies = async ({ query }: any) => {
+  const endpoint = query
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+  const res = await fetch(endpoint, {
+    method: "GET",
+    headers: TMDB_CONFIG.headers,
+  });
+
+  if (!res.ok) {
+    //@ts-ignore
+    throw new Error("Failed to fetch movies", res.statusText);
+  }
+
+  const data = await res.json();
+  return data.results;
+};
